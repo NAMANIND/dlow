68e37848280dc24e44a29783eb7f483abedc879b
Naman rai <namanrai309@gmail.com>
2024-07-15 23:40:45 +0530

    go

--- src/components/AccountLogin/AccountLogin.js
+++ src/components/AccountLogin/AccountLogin.js
@@ -42,8 +42,6 @@ export default function AccountLogin({ setIsLogin }) {
           return data;
         });
 
-      alert(userData);
-
       if (userData && userData.password === password) {
         // Example secret key (replace with your generated key)
         const secretKey = "GENERATED_SECRET_KEY_HERE";
